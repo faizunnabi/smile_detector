9797596ce36a2d6b07eb7a9db3c919281a467d54
faizunnabi <faizunnabi1990@gmail.com>
2017-11-26 16:29:15 +0400

    second commit

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ cap = cv2.VideoCapture(0)
 while 1:
     ret, img = cap.read()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (21, 21), 0)
     faces = face_classifier.detectMultiScale(gray, 1.3, 5)
     for (x, y, w, h) in faces:
         cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 1)
